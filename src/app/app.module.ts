fe919ce04a57321536511a82cb5befac10ffa8d3
Ettore Luigi Pane <ettore@ettore.one>
2022-08-21 19:56:37 +0200

    Alpha app v0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { MainCalculusComponent } from './main-calculus/main-calculus.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MainSelectorComponent } from './main-selector/main-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainCalculusComponent,
+    FileUploadComponent,
+    MainSelectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
